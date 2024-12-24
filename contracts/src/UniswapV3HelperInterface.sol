2a0c545521c98f4c53675d9385440cd345e2913a
scaraven <sc4raven@gmail.com>
2024-12-24 13:35:57 +0000

    INIT: Initialise foundry subproject

---
+++ contracts/src/UniswapV3HelperInterface.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface UniswapV3Helper {
+    function approveToken(address token, uint256 amount) external;
+    function increaseLiquidity(
+        address token0,
+        address token1,
+        uint24 fee,
+        uint256 amount0,
+        uint256 amount1,
+        int24 tickLower,
+        int24 tickUpper
+    ) external returns (uint256 tokenId, uint128 liquidity, uint256 amount0Used, uint256 amount1Used);
+}
