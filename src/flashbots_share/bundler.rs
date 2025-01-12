319c9a04e7f8c4ddeb2197cc255069c2620c5ae6
scaraven <sc4raven@gmail.com>
2025-01-12 14:00:32 +0000

    FEAT: Generalise bundler to take in multiple frontrun and backrun txs

--- src/flashbots_share/bundler.rs
+++ src/flashbots_share/bundler.rs
@@ -8,36 +8,60 @@ use alloy::{
 };
 
 use eyre::Result;
+use tokio_stream::StreamExt;
 
 pub async fn create_bundle(
     wallet: EthereumWallet,
-    frontrun: TransactionRequest,
+    frontrun: Vec<TransactionRequest>,
     sandwich: RpcTransaction,
-    backrun: TransactionRequest,
+    backrun: Vec<TransactionRequest>,
     block_number: u64,
 ) -> Result<SendBundleRequest> {
-    // Sign frontrun and backrun transaction and convert into bytes
-    let signed_front = frontrun.build(&wallet).await?.input().clone();
-    let signed_back = backrun.build(&wallet).await?.input().clone();
+    // Sign frontrun and backrun transaction and convert into vector of bytes
+    // If any single transaction fails, the entire function will fail
+    let signed_front = tokio_stream::iter(frontrun)
+        .then(|tx| async {
+            tx.build(&wallet)
+                .await
+                .map(|sig| sig.input().clone())
+                .map_err(eyre::Report::new)
+        })
+        .collect::<Result<Vec<_>>>()
+        .await?;
+    let signed_back = tokio_stream::iter(backrun)
+        .then(|tx| async {
+            tx.build(&wallet)
+                .await
+                .map(|sig| sig.input().clone())
+                .map_err(eyre::Report::new)
+        })
+        .collect::<Result<Vec<_>>>()
+        .await?;
 
     // Receive the sandwich transaction and convert into bytes
     let sandwich_hash = sandwich.input().clone();
 
-    // Create Bundle Items
-    let bundle_items = vec![
-        BundleItem::Tx {
-            tx: signed_front,
+    // Create a vector of BundleItems
+    let mut bundle_items: Vec<BundleItem> = Vec::new();
+
+    for tx in signed_front {
+        bundle_items.push(BundleItem::Tx {
+            tx,
             can_revert: false,
-        },
-        BundleItem::Tx {
-            tx: sandwich_hash,
+        });
+    }
+
+    bundle_items.push(BundleItem::Tx {
+        tx: sandwich_hash,
+        can_revert: false,
+    });
+
+    for tx in signed_back {
+        bundle_items.push(BundleItem::Tx {
+            tx,
             can_revert: false,
-        },
-        BundleItem::Tx {
-            tx: signed_back,
-            can_revert: false,
-        },
-    ];
+        });
+    }
 
     let bundle = SendBundleRequest::new(
         block_number,
